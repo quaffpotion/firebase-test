8dc0b373c79d9e641b7cd0e3ce85ba3be7442f5a
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2020-03-10 22:14:27 -0500

    Minimal firebase setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+// import "firebase/firestore";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'firebase-test';
+  constructor(private afs: AngularFirestore) {}
+
+  incrementScore() {
+    this.afs
+      .collection("books")
+      .add({
+        name: "Keanu Reeves",
+        score: 5
+      })
+      .catch(e => console.log("Error adding document: ", e));
+  }
+
+  getBook() {
+    this.afs
+      .collection("users", ref => ref.where("name", "==", "Tom Hanks"))
+      .valueChanges()
+      .subscribe(console.log);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { AngularFirestore } from "@angular/fire/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDCQwFSghWhpu09KZO0B7SOomc-v8PSt68",
+  authDomain: "fir-test-3041f.firebaseapp.com",
+  databaseURL: "https://fir-test-3041f.firebaseio.com",
+  projectId: "fir-test-3041f",
+  storageBucket: "fir-test-3041f.appspot.com",
+  messagingSenderId: "43851972851",
+  appId: "1:43851972851:web:dfbd6adad9149ab82df786",
+  measurementId: "G-SZX2VY6X9E"
+};
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
